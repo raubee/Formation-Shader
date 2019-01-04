a0895849f12c273ff7b506d94eabd65039a2bd40
adLucem <lois.aubree@gmail.com>
2019-01-04 18:54:26 +0100

    UPDATE : Add compute shader sample WIP

---
+++ Assets/Exemple 7 - Compute Shaders/Scripts/ComputeShaderMeshDampling.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class ComputeShaderMeshDampling : MonoBehaviour
+{
+    [SerializeField] private float _Speed;
+    [SerializeField] private ComputeShader _cs;
+    [SerializeField] private Material _material;
+
+    private Mesh _mesh;
+
+    private int _kernelId;
+    private uint _kernelX;
+    private uint _kernelY;
+    private uint _kernelZ;
+
+    private const int STRIDE_FLOAT3 = sizeof(float) * 3;
+
+    public static class MyStrings
+    {
+        public const string KernelName = "CSMain";
+        public const string Time = "Time";
+        public const string Speed = "Speed";
+        public const string Vertices = "Vertices";
+        public const string Normals = "Normals";
+    }
+
+    private ComputeBuffer _verticesBuffer;
+    private ComputeBuffer _normalsBuffer;
+
+    private void Awake()
+    {
+        _mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    private void Start()
+    {
+        _kernelId = _cs.FindKernel(MyStrings.KernelName);
+
+        _cs.GetKernelThreadGroupSizes(_kernelId, out _kernelX, out _kernelY, out _kernelZ);
+
+        Debug.LogFormat("[ComputeShaderDampling] Kernel Id {0}, size : ( X : {1}, Y : {2}, Z : {3} )", _kernelId, _kernelX, _kernelY, _kernelZ);
+
+        _verticesBuffer = new ComputeBuffer(_mesh.vertexCount, STRIDE_FLOAT3);
+        _normalsBuffer = new ComputeBuffer(_mesh.vertexCount, STRIDE_FLOAT3);
+
+        Debug.LogFormat("[ComputeShaderDampling] Vertices count : {0}", _mesh.vertexCount);
+
+        _verticesBuffer.SetData(_mesh.vertices);
+        _normalsBuffer.SetData(_mesh.normals);
+
+        _cs.SetBuffer(_kernelId, MyStrings.Vertices, _verticesBuffer);
+        _cs.SetBuffer(_kernelId, MyStrings.Normals, _normalsBuffer);
+    }
+
+    private void Update()
+    {
+        _cs.SetFloat(MyStrings.Time, Time.time);
+        _cs.SetFloat(MyStrings.Speed, _Speed);
+
+        _cs.Dispatch(_kernelId, (int)(_mesh.vertexCount / _kernelX), (int)_kernelY, (int)_kernelZ);
+
+        Vector3[] vertices = new Vector3[_mesh.vertexCount];
+        _verticesBuffer.GetData(vertices);
+
+        _mesh.vertices = vertices;
+    }
+
+    private void OnDestroy()
+    {
+        _verticesBuffer.Release();
+        _normalsBuffer.Release();
+
+        _verticesBuffer.Dispose();
+        _normalsBuffer.Dispose();
+    }
+}
---
+++ Assets/Exemple 7 - Compute Shaders/Scripts/ComputeShaderTest.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class ComputeShaderTest : MonoBehaviour
+{
+    [SerializeField] private Vector2Int _textureSize;
+    [SerializeField] private float _Speed;
+    [SerializeField] private ComputeShader _cs;
+    [SerializeField] private Material _material;
+
+    private RenderTexture _renderTexture;
+    private int _kernelId;
+
+    private const int TEXTURE_DEPTH = 24;
+
+    private uint _kernelX;
+    private uint _kernelY;
+    private uint _kernelZ;
+
+    public static class MyStrings
+    {
+        public const string KernelName = "CSMain";
+        public const string ResultTexture = "Result";
+        public const string Time = "Time";
+        public const string Speed = "Speed";
+        public const string MaterialMainTex = "_MainTex";
+    }
+
+    private void Start()
+    {
+        // Run kernel
+        //
+        _renderTexture = new RenderTexture(_textureSize.x, _textureSize.y, TEXTURE_DEPTH)
+        {
+            enableRandomWrite = true
+        };
+
+        _renderTexture.Create();
+
+        _kernelId = _cs.FindKernel(MyStrings.KernelName);
+        _cs.SetTexture(_kernelId, MyStrings.ResultTexture, _renderTexture);
+
+        _cs.GetKernelThreadGroupSizes(_kernelId, out _kernelX, out _kernelY, out _kernelZ);
+        Debug.LogFormat("[ComputeShaderTest] Kernel Id {0} size : ( X : {1}, Y : {2}, Z : {3} )", _kernelId, _kernelX, _kernelY, _kernelZ);
+
+        _material.SetTexture(MyStrings.MaterialMainTex, _renderTexture);
+    }
+
+    private void Update()
+    {
+        _cs.SetFloat(MyStrings.Time, Time.time);
+        _cs.SetFloat(MyStrings.Speed, _Speed);
+        _cs.Dispatch(_kernelId, (int)(_textureSize.x / _kernelX), (int)(_textureSize.y / _kernelY), (int)_kernelZ);
+    }
+
+    private void OnDestroy()
+    {
+        _renderTexture.Release();
+        DestroyImmediate(_renderTexture);
+    }
+}
